63f0a1d8c892f7b0910f1bfd10dd243a2a15060c
Kevin Hughes <hughesk1827@gmail.com>
2023-11-29 21:59:58 -0600

    Added items list to the Stop class and commented the data

--- app/src/main/java/com/cs407/errandadventures/Stop.java
+++ app/src/main/java/com/cs407/errandadventures/Stop.java
@@ -1,7 +1,10 @@
 package com.cs407.errandadventures;
 
+import java.util.ArrayList;
+
 public class Stop {
-    String name;
-    String address;
+    String name; // name of the stop displayed to user in the story
+    String address; // address of the stop for maps
+    ArrayList<String> items; // items to be found at the stop (the errands so to speak)
     int id;
 }
