686f0dc60daa1562d53c7863e98d16efc5391dfd
ThomasZelichowski <tzelic2@illinois.edu>
2023-12-05 17:06:53 -0600

    Added map (only displays current location for now)

--- app/src/main/java/com/cs407/errandadventures/mapFragment.java
+++ app/src/main/java/com/cs407/errandadventures/mapFragment.java
@@ -1,77 +1,86 @@
 package com.cs407.errandadventures;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link mapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 public class mapFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private final LatLng mDestinationLatLng = new LatLng(43.0757, -89.4040);
+    private GoogleMap mMap;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
-
-    public mapFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment mapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static mapFragment newInstance(String param1, String param2) {
-        mapFragment fragment = new mapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment=(SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map_fragment);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+            displayMyLocation();
+        });
+        mFusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(getActivity());
+
+        // Return view 
+        return view;
+    }
+
+    private void displayMyLocation() {
+        int permission = ActivityCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(getActivity(), task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        System.out.println(mLastKnownLocation);
+                        if (task.isSuccessful() && mLastKnownLocation != null) {
+                            mMap.addPolyline(new PolylineOptions().add(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()),
+                                    mDestinationLatLng));
+                            mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()))
+                                    .title("Start"));
+                        }
+                    });
+        }
     }
 
     @Override
-    public void onRequestPermissionsResult (int code, @NonNull String[] permissions, @NonNull int[] result) {
-        super.onRequestPermissionsResult(code, permissions, result);
-        //if (code == ACCESS_LOCATION) {
-            if (result.length > 0 && result[0] == PackageManager.PERMISSION_GRANTED) {
-                //displayLoc();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
             }
-        //}
+        }
     }
+
 }
