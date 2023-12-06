90896100e5e80a39ab71a9bb35cbec8b5c20a63e
ThomasZelichowski <tzelic2@illinois.edu>
2023-12-05 20:19:40 -0600

    Progress on Google Places API to set locations.

--- app/src/main/java/com/cs407/errandadventures/AddStop.java
+++ app/src/main/java/com/cs407/errandadventures/AddStop.java
@@ -1,5 +1,8 @@
 package com.cs407.errandadventures;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,7 +11,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AddStop extends AppCompatActivity {
 
@@ -24,7 +37,33 @@ public class AddStop extends AppCompatActivity {
         Intent intent = getIntent();
         username = intent.getStringExtra("username");
 
+        Places.initialize(getApplicationContext(), "AIzaSyCexdL1RcVZxgHsJSg2jeaK889V0EgQL6g");
+
     }
+
+    public void startAutocompleteActivity(View view) {
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+                .build(this);
+        startAutocomplete.launch(intent);
+    }
+
+    private final ActivityResultLauncher<Intent> startAutocomplete = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent intent = result.getData();
+                    if (intent != null) {
+                        Place place = Autocomplete.getPlaceFromIntent(intent);
+                        Log.i(TAG, "Place: ${place.getName()}, ${place.getId()}");
+                    }
+                } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                    // The user canceled the operation.
+                    Log.i(TAG, "User canceled autocomplete");
+                }
+            });
 
     public void done(View view) {
 
