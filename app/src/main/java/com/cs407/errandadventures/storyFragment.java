ab91efec7ba1a875f93ad5645388e4e4471ca739
Kevin Hughes <hughesk1827@gmail.com>
2023-12-06 23:08:15 -0600

    Changes: - Refactoring to storyFragment - Added function to generate body paragraphs - Added function to change stages - Started but didn't finish updating story based on user location

--- app/src/main/java/com/cs407/errandadventures/storyFragment.java
+++ app/src/main/java/com/cs407/errandadventures/storyFragment.java
@@ -1,5 +1,6 @@
 package com.cs407.errandadventures;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,10 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 
@@ -28,62 +33,90 @@ public class storyFragment extends Fragment {
     // each paragraph is separated into their own string.  This should make it easier to reveal
     // paragraphs one at a time as the user progresses through the story
     private ArrayList<String> body;
+    private int bodyIdx = 0;
     // ending paragraph
     private String ending;
+
+    // list of the stops
+    private ArrayList<Stop> stops;
 
     // the text displayed to the user
     private TextView text;
 
+    // location provider
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // generate new text if we haven't already and we need it
-        switch (stage) {
-            case INTRO:
-                // intro will be generated here
-                if (intro == null || intro.isEmpty()) {
-                    intro = "PLACEHOLDER INTRO";
-                }
-                break;
-            case BODY:
-                // body will be generated here
-                if (body == null) {
-                    body = new ArrayList<>();
-                    body.add("PLACEHOLDER BODY 1");
-                    body.add("PLACEHOLDER BODY 2");
-                    body.add("PLACEHOLDER BODY 3");
-                }
-                break;
-            case ENDING:
-                // ending will be generated here
-                if (ending == null || ending.isEmpty()) {
-                    ending = "PLACEHOLDER ENDING";
-                }
-                break;
-        }
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_story, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        text = view.findViewById(R.id.storyText);
+        // TODO: update the story based on the user's location.  Need to implement callback
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        LocationRequest request = new LocationRequest.Builder(1000).build(); // update every second
+
+        // get the stops from the database
+        Context context = getActivity().getApplicationContext();
+        DBHelper helper = new DBHelper(context.openOrCreateDatabase("toDo", Context.MODE_PRIVATE, null));
+        stops = helper.readList(context.getSharedPreferences("com.cs407.errandadventures", Context.MODE_PRIVATE).getString("username", ""));
+
+        // if stops were added, generate body paragraphs
+        if (stops.size() > 0 && (body == null || stops.size() > body.size())) {
+            genBodyParagraph();
+        }
+
         // set the text of the textview to the appropriate text
+        text = view.findViewById(R.id.storyText);
         switch (stage) {
             case INTRO:
                 // set the text to the intro
+                if (intro == null || intro.isEmpty()) {
+                    intro = "PLACEHOLDER INTRO";
+                }
                 text.setText(intro);
                 break;
             case BODY:
-                // set the text to the body
-                text.setText(body.get(0));
+                assert body != null;
+                assert body.size() <= bodyIdx && bodyIdx >= 0;
+                text.setText(body.get(bodyIdx));
                 break;
             case ENDING:
                 // set the text to the ending
+                if (ending == null || ending.isEmpty()) {
+                    ending = "PLACEHOLDER ENDING";
+                }
                 text.setText(ending);
                 break;
         }
+    }
+
+    private void changeStage() {
+        switch (stage) {
+            case INTRO:
+                stage = Stage.BODY;
+                break;
+            case BODY:
+                stage = Stage.ENDING;
+                break;
+            case ENDING:
+                stage = Stage.INTRO;
+                break;
+        }
+    }
+
+    private void genBodyParagraph() {
+        if (body == null) {
+            body = new ArrayList<>();
+        }
+
+        // add body paragraphs for each new stop\
+        // we assume that there are fewer body paragraphs than stops
+        for (int i = body.size(); i < stops.size(); i++) {
+            body.add("PLACEHOLDER BODY " + (i + 1));
+        }
+
     }
 
 }
